c81d343d97114e8885e8a7cadee02855fa247785
Samuel Toledo <samuel.toledo@yh.nackademin.s>
2023-10-26 13:51:11 +0200

    Hejsan vänner!

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Mittnamn samuel = new Mittnamn("Samuel","Toledo",29);
+    }
+}
---
+++ src/Mittnamn.java
@@ -0,0 +1,25 @@
+public class Mittnamn {
+
+    String namn;
+    String efterNamn;
+    int ålder;
+
+    public Mittnamn(String namn,String efterNamn,int ålder) {
+        this.namn = namn;
+        this.efterNamn = efterNamn;
+        this.ålder = ålder;
+
+
+    }
+
+    public String getNamn() {
+        return namn;
+    }
+    public String getEfterNamn() {
+        return efterNamn;
+
+    }
+    public int getÅlder() {
+        return ålder;
+    }
+}
